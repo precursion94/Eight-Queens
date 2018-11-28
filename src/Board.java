5e3d1f2630babe7f03e27216261efd1ca77eeab6
recursor94 <werdnaonaps@gmail.com>
2018-11-27 21:17:53 -0500

    added row col to queen and added testing for collisions in board

--- src/Board.java
+++ src/Board.java
@@ -2,21 +2,153 @@ public class Board {
     private Queen[][] board;
 
     public Board(String coordinateString) {
+        setBoardState(coordinateString);
 
-        this.board = new Queen[8][8];
-        for(int col = 0; col < 8; col++) {
-            int row = Integer.parseInt(coordinateString.charAt(col) + "");
-            board[row][col] = new Queen();
-        }
     }
     public Queen[][] getBoard() {
         return board;
     }
 
-    public boolean testNewMove(String coordinateString) {
-       return false;
+    public void testNewState(String coordinateString) {
+        setBoardState(coordinateString);
 
+        for(int r = 0; r < board.length; r++) {
+            for(int c = 0; c < board.length; c++) {
+                boolean collisionExists = testQueenCollision(board[r][c]);
+                if(collisionExists) {
+
+                    System.out.println("Collision at: (" + r + ", " + c + ")");
+                    return;
+                }
+            }
+        }
     }
 
-    //private boolean testUp(
+    public boolean testQueenCollision(Queen queen) {
+        if(!testUp(queen))
+            return false;
+        else if (!testDown(queen))
+            return false;
+        else if (!testRight(queen))
+            return false;
+        else if (!testLeft(queen))
+            return false;
+        else if(!testUpRight(queen))
+            return false;
+        else if (!testUpLeft(queen))
+            return false;
+        else if (!testDownRight(queen))
+            return false;
+        else if (!testDownLeft(queen))
+            return false;
+
+        return true;
+    }
+//  true = no collisions; false = collision
+    public boolean testUp(Queen queen) {
+        if(queen.getRow() == 7) {
+            return true;
+        }
+        for(int r = queen.getRow(); r < 8; r++) {
+            if(board[r][queen.getCol()] != null)
+                return false;
+        }
+        return true;
+    }
+    public boolean testDown(Queen queen) {
+        if(queen.getRow() == 0) {
+            return true;
+        }
+        for(int r = queen.getRow(); r >= 0; r--) {
+            if(board[r][queen.getCol()] != null)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean testRight(Queen queen) {
+        if(queen.getCol() == 7) {
+            return true;
+        }
+       for(int c = queen.getCol(); c < 8; c++) {
+            if(board[queen.getRow()][c] != null)
+                return false;
+        }
+        return true;
+    }
+
+    public boolean testLeft(Queen queen) {
+        if(queen.getCol() == 0) {
+            return true;
+        }
+        for(int c = queen.getCol(); c >= 0; c--) {
+            if(board[queen.getRow()][c] != null)
+                return false;
+        }
+        return true;
+    }
+    public boolean testUpRight(Queen queen) {
+        if(queen.getRow() == 7 || queen.getCol() == 7) {
+            return true;
+        }
+        for(int r = queen.getRow(); r < 8; r++) {
+            for(int c = queen.getCol(); c < 8; c++) {
+                if(board[r][c] != null)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+
+    public boolean testUpLeft(Queen queen) {
+
+        if(queen.getRow() == 7 || queen.getCol() == 0) {
+            return false;
+        }
+        for(int r = queen.getRow(); r < 8; r++) {
+            for(int c = queen.getCol(); c >= 0; c--) {
+                if(board[r][c] !=null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean testDownRight(Queen queen) {
+        if(queen.getRow() == 0 || queen.getCol() == 7) {
+            return true;
+        }
+        for(int r = queen.getRow(); r >= 0; r--) {
+            for(int c = queen.getCol(); c < 8; c++) {
+                if(board[r][c] != null)
+                    return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean testDownLeft (Queen queen) {
+
+        if(queen.getRow() == 0 || queen.getCol() == 0) {
+            return false;
+        }
+        for(int r = queen.getRow(); r >= 0; r--) {
+            for(int c = queen.getCol(); c >= 0; c--) {
+                if(board[r][c] !=null) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void setBoardState(String coordinateString) {
+        this.board = new Queen[8][8];
+        for(int col = 0; col < 8; col++) {
+            int row = Integer.parseInt(coordinateString.charAt(col) + "");
+            board[row][col] = new Queen(row,col);
+        }
+    }
 }
--- src/Queen.java
+++ src/Queen.java
@@ -1,2 +1,16 @@
 public class Queen {
+    private int row;
+    private int col;
+
+    public Queen(int row, int col) {
+        this.row  = row;
+        this.col = col;
+    }
+
+   public int getRow() {
+        return row;
+   }
+   public int getCol() {
+        return col;
+   }
 }
